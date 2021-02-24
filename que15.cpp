78645c6d8d8b68e225579bcfcc83d36c2948a5bf
Adarsh-11 <56095903+Adarsh-11@users.noreply.github.com>
2021-02-25 00:43:58 +0530

    Update que15.cpp

--- que15.cpp
+++ que15.cpp
@@ -2,34 +2,38 @@
 using namespace std;
 int main()
 {
-	int n,i,k,j,q;
-	cin>>n;
-	for(i=0;i<n;i++)
-	{
-			for(j=0;j<n-i-1;j++)
-	{
-		cout<<" ";
-	}
-	for(k=0;k<2*i+1;k++)
-	{
-		cout<<k+1;
-	}
-	cout<<endl;
-	}
-	q=0;
-	for(i=n-1;i>0;i--)
-	{
-			for(j=0;j<q+1;j++)
-	{
-		cout<<" ";
-	}
-	
-	for(k=0;k<=2*q-1;k++)
-	{
-		cout<<k+1;
-		q++;
-	}
-	cout<<endl;
-	}
-	return 0;
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+
+    int i,j,k;
+    for(i=1;i<=n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<(i*2);k++)
+        {
+            cout<<k;
+        }
+        cout<<"\n";
+    }
+
+    for(i=n-1;i>=1;i--)
+    {
+        for(j=n;j>i;j--)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<(i*2);k++)
+        {
+            cout<<k;
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
+
+
+
